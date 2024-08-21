fb4ac99805ad023ba2c23fdb43ba011d804b694d
okluan <luanemerson8@gmail.com>
2024-08-21 10:27:19 -0300

    Alterações

--- 1_exemplo/app.js
+++ 1_exemplo/app.js
@@ -5,5 +5,27 @@ const db = new sqlite3.Database("./test.db", sqlite3.OPEN_READWRITE, (err) =>{
     if (err) return console.error(err.message)
 });
 
-sql = "CREATE TABLE users(id integer primary key, first_name, last_name,username, password, email)";
-db.run(sql);
+//Create table
+//sql = "CREATE TABLE users(id integer primary key, first_name, last_name,username, password, email)";
+//db.run(sql);
+
+//Drop Table
+//db.run("DROP TABLE users")
+
+//Insert data into table
+//sql = "INSERT INTO users(first_name, last_name, username, password, email) VALUES (?,?,?,?,?)";
+//db.run(sql, ["mike", "michel", "mike_user", "test", "mike@gmail.com"], (err) =>{
+//   if (err) return console.error(err.message);
+
+//})
+
+//Select all
+//sql = "select * from users";
+//db.all(sql, [], (err, rows) => {
+//    if (err) return console.error(err.message);
+//    rows.forEach((row) => {
+//        console.log(row)
+//    });
+//})
+
+//Update data
